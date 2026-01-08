8f302ccf34052994e334b2a81d8ae1257a531f3a
abdul-manan24 <manan.cs.2k24@gmail.com>
2026-01-09 03:29:15 +0500

    second commit

---
+++ Input_Output_Exercises/Ex_I.py
@@ -0,0 +1,8 @@
+# __Challenge__
+
+# Write a program to accept two integer numbers from the user and calculate their product.
+
+num1 = int(input("Enter 1st num: "))
+num2 = int(input("Enter 2nt num: "))
+
+print(f"Product is {num1 * num2}")
