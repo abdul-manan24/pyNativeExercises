26ea493bba08b3ab9b4ee20b0b6b372e019bd0b5
abdul-manan24 <manan.cs.2k24@gmail.com>
2026-02-14 22:58:38 +0500

    Pushing one solved exercise

--- Py_OOP_Exercises/Ex_A.py
+++ Py_OOP_Exercises/Ex_A.py
@@ -2,3 +2,12 @@
 
 # Write a Python program to create a Vehicle class
 # with max_speed and mileage instance attributes.
+
+class Vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+modelX = Vehicle(240, 18)
+print(f"Max Speed: {modelX.max_speed}")
+print(f"Mileage: {modelX.mileage}")
